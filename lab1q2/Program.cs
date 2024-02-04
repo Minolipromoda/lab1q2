83f99e0673668b924e1032eaed1b5bbf1e326c74
MINOLI <MINOLI@LAPTOP-VTQD26BJ>
2024-02-05 00:04:32 +0530

    Add project files.

---
+++ lab1q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 10 numbers");
+            for (int i = 0; i <10; i++) 
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine($"eneter number {i+1}:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                string result = (number % 2 == 0) ? "even " : "odd";
+                Console.WriteLine($"the number {number} is { result} ");
+            }
+        }
+    }
+}
